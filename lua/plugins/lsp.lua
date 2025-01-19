27af0ba73c449720ab57039c56b877db6742ed15
Eddy <evermoen@gmail.com>
2025-01-19 20:10:06 +0100

    Change code completion mapping to C-C

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -108,7 +108,7 @@ return {
         end, { "i", "s" }),
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-C>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
         ["<C-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
       }),
