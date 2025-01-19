49534e63515f8e93564b6cf2497636a681d830c7
Eddy <evermoen@gmail.com>
2025-01-19 12:00:51 +0100

    Replace fidget with snacks, add ruff

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,24 +11,9 @@ return {
     "saadparwaiz1/cmp_luasnip",
     "rafamadriz/friendly-snippets",
     "onsails/lspkind.nvim",
-    {
-      "j-hui/fidget.nvim",
-      opts = {
-        progress = {
-          display = {
-            done_icon = '✓', -- Icon shown when all LSP progress tasks are complete
-          },
-        },
-        notification = {
-          window = {
-            winblend = 0, -- Background color opacity in the notification window
-          },
-        },
-      },
-    },
   },
   config = function()
-    local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+    local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
     local servers = {
       bashls = {},
@@ -36,18 +21,17 @@ return {
         settings = {
           Lua = {
             diagnostics = {
-              globals = { 'vim' }
+              globals = { "vim" },
             },
           },
         },
       },
       powershell_es = {
-        bundle_path = 'c:/Software/Dev/PowerShellEditorServices/',
-        capabilities = lsp_capabilities,
+        bundle_path = "c:/Software/Dev/PowerShellEditorServices/",
         settings = {
           powershell = {
             codeFormatting = {
-              Preset = 'Allman'
+              Preset = "Allman",
             },
           },
         },
@@ -71,53 +55,30 @@ return {
       ruff = {
         -- Notes on code actions: https://github.com/astral-sh/ruff-lsp/issues/119#issuecomment-1595628355
         -- Get isort like behavior: https://github.com/astral-sh/ruff/issues/8926#issuecomment-1834048218
-        commands = {
-          RuffAutofix = {
-            function()
-              vim.lsp.buf.execute_command {
-                command = 'ruff.applyAutofix',
-                arguments = {
-                  { uri = vim.uri_from_bufnr(0) },
-                },
-              }
-            end,
-            description = 'Ruff: Fix all auto-fixable problems',
-          },
-          RuffOrganizeImports = {
-            function()
-              vim.lsp.buf.execute_command {
-                command = 'ruff.applyOrganizeImports',
-                arguments = {
-                  { uri = vim.uri_from_bufnr(0) },
-                },
-              }
-            end,
-            description = 'Ruff: Format imports',
-          },
-        },
+        commands = {},
       },
     }
 
-    require('mason').setup()
+    require("mason").setup()
 
     local ensure_installed = vim.tbl_keys(servers or {})
 
-    require('mason-lspconfig').setup {
+    require("mason-lspconfig").setup({
       ensure_installed = ensure_installed,
       handlers = {
         function(server_name)
           local server = servers[server_name] or {}
           server.capabilities = lsp_capabilities
-          require('lspconfig')[server_name].setup(server)
+          require("lspconfig")[server_name].setup(server)
         end,
       },
-    }
+    })
 
-    local lspkind = require('lspkind')
+    local lspkind = require("lspkind")
 
     --Prettyfy with icons
     --
-    lspkind.init {}
+    lspkind.init({})
 
     -- Snippets
     --
@@ -126,14 +87,14 @@ return {
     -- Autocomplete
     --
 
-    local cmp = require('cmp')
-    local luasnip = require('luasnip')
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
 
     cmp.setup({
       snippet = {
         expand = function(args)
           luasnip.lsp_expand(args.body)
-        end
+        end,
       },
       mapping = cmp.mapping.preset.insert({
         ["<Tab>"] = cmp.mapping(function(fallback)
@@ -145,21 +106,21 @@ return {
             fallback()
           end
         end, { "i", "s" }),
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<C-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
       }),
       sources = {
-        { name = 'nvim_lsp', keyword_length = 1 },
-        { name = 'luasnip',  keyword_length = 1 },
-        { name = 'buffer',   keyword_length = 2 },
-        { name = 'path',     keyword_length = 3 }
+        { name = "nvim_lsp", keyword_length = 1 },
+        { name = "luasnip",  keyword_length = 1 },
+        { name = "buffer",   keyword_length = 2 },
+        { name = "path",     keyword_length = 3 },
       },
       window = {
         documentation = cmp.config.window.bordered(),
-        completion = cmp.config.window.bordered()
+        completion = cmp.config.window.bordered(),
       },
       formatting = {
         format = lspkind.cmp_format(),
@@ -177,5 +138,5 @@ return {
         luasnip.jump(-1)
       end
     end, { silent = true })
-  end -- end config
+  end, -- end config
 }
