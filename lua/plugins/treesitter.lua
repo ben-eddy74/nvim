3e93fb8451925bd79a4406333003dc20b0dace50
Eddy <evermoen@gmail.com>
2025-01-19 12:01:59 +0100

    Extend ensure_installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,32 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
+  "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
     local configs = require("nvim-treesitter.configs")
     configs.setup({
-      ensure_installed = { 'vimdoc', 'lua', 'bash' },
+      ensure_installed = {
+        "bash",
+        "css",
+        "dockerfile",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "powershell",
+        "python",
+        "regex",
+        "sql",
+        "vimdoc",
+        "xml",
+        "yaml",
+      },
       auto_install = true,
       highlight = {
         enable = true,
-        additional_vim_regex_highlighting = false
-      }
+        additional_vim_regex_highlighting = false,
+      },
     })
-  end
+  end,
 }
