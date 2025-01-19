99381243e59b8f7c71d53563ce2de028cbbad1bc
Eddy <evermoen@gmail.com>
2025-01-19 12:02:14 +0100

    Add snacks plugin

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    notifier = {
+      enabled = true,
+    },
+    indent = {
+      enabled = true,
+      only_scope = true,
+    },
+  },
+}
