36175d316618890d80bf9ab447f6c5dc537065de
Eddy <evermoen@gmail.com>
2025-02-10 08:54:00 +0100

    Add code completion plugin

---
+++ lua/plugins/codecompletion.lua
@@ -0,0 +1,37 @@
+return {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    require("codecompanion").setup({
+      adapters = {
+        llama3 = function()
+          return require("codecompanion.adapters").extend("ollama", {
+            name = "llama3", -- Give this adapter a different name to differentiate it from the default ollama adapter
+            schema = {
+              model = {
+                default = "mistralt",
+              },
+              num_ctx = {
+                default = 16384,
+              },
+              num_predict = {
+                default = -1,
+              },
+            },
+          })
+        end,
+      },
+      strategies = {
+        chat = {
+          adapter = "ollama",
+        },
+        inline = {
+          adapter = "ollama",
+        },
+      },
+    })
+  end,
+}
