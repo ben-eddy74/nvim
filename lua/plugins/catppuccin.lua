453434dc3fd0d87312b0a42d10d18f56ba331d68
Eddy <evermoen@gmail.com>
2025-03-25 20:04:40 +0100

    Transparent background

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,18 +1,19 @@
 return {
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        opts = {
-            flavour = "frappe",
-            integrations = {
-                cmp = true,
-                treesitter = true,
-            },
-        },
-        config = function(_, opts)
-            require("catppuccin").setup(opts)
-            vim.cmd.colorscheme("catppuccin")
-        end,
-    },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			flavour = "frappe",
+			transparent_background = true,
+			integrations = {
+				cmp = true,
+				treesitter = true,
+			},
+		},
+		config = function(_, opts)
+			require("catppuccin").setup(opts)
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
 }
