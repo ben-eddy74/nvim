ae670cc48e746bb2f2086eb9836b67c09b6b9221
Eddy <evermoen@gmail.com>
2025-12-28 12:55:34 +0100

    Set SessionDetailsPath to project folder; Change code formatting options

--- lsp/powershell.lua
+++ lsp/powershell.lua
@@ -1,23 +1,30 @@
 return {
 	cmd = {
-		"pwsh",
+		"powershell",
 		"-NoLogo",
 		"-NoProfile",
 		"-Command",
 		PSes_path .. "\\PowerShellEditorServices\\Start-EditorServices.ps1",
 		"-BundledModulesPath " .. PSes_path,
 		"-stdio",
-		"-SessionDetailsPath $ENV:USERPROFILE/pwessession.json",
+		"-SessionDetailsPath ./psessession.json",
+		"-LogLevel 'Information'",
 	},
 	settings = {
 		bundle_path = Home_directory .. "/AppData/local/nvim-data/mason/packages/powershell-editor-services",
 		powershell = {
-			codeFormatting = { Preset = "OTBS" },
+			codeFormatting = {
+				-- Preset = "OTBS",
+				useCorrectCasing = true,
+				autoCorrectAliases = true,
+				openBraceOnSameLine = false,
+				pipelineIndentationStyle = "IncreaseIndentationForFirstPipeline",
+			},
 			init_options = {
 				enableProfileLoading = false,
 			},
 		},
 	},
-	filetypes = { "ps1", "psd1" },
+	filetypes = { "ps1", "psd1", "psm1" },
 	root_markers = { "requiredmodules.psd1" },
 }
