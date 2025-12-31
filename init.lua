19f05606c8bbc61518216fa575ae8f882068531c
Eddy <evermoen@gmail.com>
2025-12-31 19:34:47 +0100

    Increase lazy updates check to notify every 4 days

--- init.lua
+++ init.lua
@@ -37,7 +37,7 @@ require("lazy").setup({
 		{ import = "plugins" },
 	},
 	-- automatically check for plugin updates
-	checker = { enabled = false, notify = true, frequency = 345600 },
+	checker = { enabled = true, notify = true, frequency = 345600 },
 	-- Disable hererocks
 	opts = { rocks = { enabled = false } },
 })
