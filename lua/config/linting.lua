ed78f11cafadb9196b0c5f7d262f21cdb6d320d3
Eddy <evermoen@gmail.com>
2026-01-25 13:17:56 +0100

    Disable line length rule for Markdown

--- lua/config/linting.lua
+++ lua/config/linting.lua
@@ -1,18 +1,23 @@
 local lint = require("lint")
 
+local mdlint = lint.linters.markdownlint
+mdlint.args = {
+  "--stdin", "--disable", "MD013", "--"
+}
+
 lint.linters_by_ft = {
-	markdown = { "markdownlint" },
-	--python = { "ruff" },
-	typescript = { "eslint_d" },
-	javascript = { "eslint_d" },
+  markdown = { "markdownlint" },
+  --python = { "ruff" },
+  typescript = { "eslint_d" },
+  javascript = { "eslint_d" },
 }
 
 local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
 -- Try linting
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-	group = lint_augroup,
-	callback = function()
-		lint.try_lint()
-	end,
+  group = lint_augroup,
+  callback = function()
+    lint.try_lint()
+  end,
 })
