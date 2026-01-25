304dc676d449df947ef88019354d748f50001bd3
Eddy <evermoen@gmail.com>
2026-01-25 10:20:46 +0100

    Add markview plugin

---
+++ lua/plugins/markview.lua
@@ -0,0 +1,11 @@
+-- https://github.com/OXY2DEV/markview.nvim
+return {
+  "OXY2DEV/markview.nvim",
+  lazy = false,
+  opts = {
+    preview = {
+      filetypes = { "markdown", "codecompanion" },
+      ignore_buftypes = {},
+    },
+  },
+};
