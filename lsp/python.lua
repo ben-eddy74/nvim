8b853ba6a910b1095625cc10816ef172ee8b40ff
Eddy Vermoen <eddy.vermoen@devops.ncia.nato.int>
2026-03-20 08:20:00 +0100

    Migrate to pylsp

--- lsp/python.lua
+++ lsp/python.lua
@@ -1,21 +1,21 @@
 return {
-	cmd = { "basedpyright-langserver", "--stdio" },
-	filetypes = { "python" },
-	root_markers = { "pyproject.toml" },
-	settings = {
-		basedpyright = {
-			enabled = true,
-			settings = {
-				disableOrganizeImports = true,
-				basedpyright = {
-					analysis = {
-						ignore = { "*" },
-						typeCheckingMode = "recommended",
-						diagnosticMode = "workspace",
-						useLibraryCodeForTypes = true,
-					},
-				},
-			},
-		},
-	},
+  cmd = { "pylsp" },
+  filetypes = { "python" },
+  root_markers = { "pyproject.toml" },
+  settings = {
+    pylsp = {
+      plugins = {
+        jedi = {
+          environment = "./.venv",
+          extra_paths = { "./src" }
+        },
+        pyflakes = { enabled = false },
+        pycodestyle = {
+          enabled = true,
+          maxLineLength = 100,
+          ignore = { "E203", "E722" },
+        },
+      }
+    }
+  },
 }
