74965bdc69d20f0594d0e8bcc742e1ca2fb8a92a
Eddy Vermoen <eddy.vermoen@devops.ncia.nato.int>
2026-07-18 23:07:57 +0200

    Add keymaps to close/delete buffers

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,10 @@
 -- nvim-tree
 vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle, { desc = "Toggle nvim-tree" })
 
+-- Close all buffers
+vim.keymap.set("n", "<leader>bda", ":bd|e#<CR>", { desc = "Delete all buffers except current" })
+vim.keymap.set("n", "<leader>bw", ":bufdo bwipeout", { desc = "Wipe out all buffers" })
+
 -- Python
 vim.keymap.set("n", "<leader>pyb", "<cmd>!uv build<CR>", { desc = "Python: Build python module (uv)" })
 
