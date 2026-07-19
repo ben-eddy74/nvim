a24cb58ef1d6d0d269b5f189fdb5fb9ab27eabca
Eddy Vermoen <eddy.vermoen@devops.ncia.nato.int>
2026-07-19 22:01:17 +0200

    Disable lsp progress messages

--- plugin/nvim-notify.lua
+++ plugin/nvim-notify.lua
@@ -10,6 +10,9 @@ require("noice").setup({
 			["vim.lsp.util.stylize_markdown"] = true,
 			["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 		},
+		progress = {
+			enabled = false,
+		},
 	},
 	presets = {
 		bottom_search = true, -- use a classic bottom cmdline for search
